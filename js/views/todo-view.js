d3c46aa06e75b70942800e39b9a0d14d632c41e9
ricardo-0x07 <ricardo_0x07@yahoo.com>
2016-03-13 01:47:59 -0400

    added priority feature

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -20,6 +20,7 @@ var app = app || {};
 			'click .toggle': 'toggleCompleted',
 			'dblclick label': 'edit',
 			'click .edit-btn': 'edit',
+			'click .priority-btn': 'togglePriority',
 			'click .destroy': 'clear',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
@@ -51,10 +52,8 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.$title = this.$('#title');
-			// if(this.model.get('completed')){
-			// 	$(this.$title).after('<label> -- Done</label>');
-			// }
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -132,6 +131,13 @@ var app = app || {};
 		// Remove the item, destroy the model from *localStorage* and delete its view.
 		clear: function () {
 			this.model.destroy();
+		},
+
+		// Toggle the `"priority"` state of the model.
+		togglePriority: function () {
+			this.$el.addClass('priority');
+			this.model.togglePriority();
+
 		}
 	});
 })(jQuery);
